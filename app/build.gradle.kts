4e7bc66858c0e6722d68882df0f26d2f1a352913
Anjishnu Nandi <anjicroma@gmail.com>
2025-10-14 08:22:48 +0530

    Update version to 2.9.232.687 Stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "chromahub.rhythm.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29230684
-        versionName = "2.9.230.684"
+        versionCode = 29232687
+        versionName = "2.9.232.687"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
